cdad3767a9f41f3711b84690f99c575d85d95076
ananth_rangarajan <35774247+rananth99@users.noreply.github.com>
2024-05-27 04:58:05 +0000

    Add cin and cout

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,19 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello, World !!"<<endl;
+    string str;
+    cout<<"What is your name?"<<flush;
+    cin>>str;
+    cout<<"Hello "<<str<<". Hope you are doing fine!!";
+    cout<<endl<<endl;
+
+    return{0};
+}
