262236913c4b8028237b165027193e714d17c553
ananth_rangarajan <35774247+rananth99@users.noreply.github.com>
2024-06-18 23:03:01 +0000

    modify files

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -6,7 +6,15 @@
 #include <string>
 
 int main(){
+    int a = 37;
+    int *ptr;
+    ptr = &a;
 
+    std::cout << "contents of variable a : "<<a<<std::endl;
+    std::cout << "pointer ptr pointing to : "<<ptr<<std::endl;
+    std::cout << "address of variable a : "<<&a<<std::endl;
+    std::cout << "contents of pointer ptr : "<<*ptr<<std::endl;
+    std::cout << "address of pointer ptr : "<<&ptr<<std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
