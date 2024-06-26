fdba2d7c8139f005f51377216198598f5affa12a
ananth_rangarajan <35774247+rananth99@users.noreply.github.com>
2024-06-26 05:42:20 +0000

    modify files

--- src/Ch05/05_02b/CodeDemo.cpp
+++ src/Ch05/05_02b/CodeDemo.cpp
@@ -4,13 +4,37 @@
 
 #include <iostream>
 
+// pass by value
+int square(int x){
+    return x * x;
+}
+
+// pass by pointers
+void swap1(int *x, int *y){
+    int temp = *x + *y;
+    *x = temp - *x;
+    *y = temp - *y;
+}
+
+// pass by references
+void swap2(int& x, int& y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+
+
 int main(){
     int a = 9, b;
     // TODO: square
+    b = square(a);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     // TODO: swap
+    swap1(&a, &b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     // TODO: swap
+    swap2(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;
